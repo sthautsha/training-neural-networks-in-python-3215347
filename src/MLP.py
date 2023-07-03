ee214790c9d8466ea60f9a666eea25980cb60a20
sthautsha <46290853+sthautsha@users.noreply.github.com>
2023-07-03 05:08:46 +0000

    first commit

--- src/MLP.py
+++ src/MLP.py
@@ -1,16 +1,22 @@
 import numpy as np
 
+
 class Perceptron:
     """A single neuron with the sigmoid activation function.
-       Attributes:
-          inputs: The number of inputs in the perceptron, not counting the bias.
-          bias:   The bias term. By default it's 1.0."""
+    Attributes:
+       inputs: The number of inputs in the perceptron, not counting the bias.
+       bias:   The bias term. By default it's 1.0."""
 
-    def __init__(self, inputs, bias = 1.0):
-        """Return a new Perceptron object with the specified number of inputs (+1 for the bias).""" 
-        pass
+    def __init__(self, inputs, bias=1.0):
+        """Return a new Perceptron object with the specified number of inputs (+1 for the bias)."""
+        # From 0 to 1 ads scaling factor of 2 and shift of -1"""
+        self.weights = (np.random.rand(inputs + 1) * 2) - 1
+        self.bias = bias
 
     def run(self, x):
         """Run the perceptron. x is a python list with the input values."""
-        pass
-        
+        # returns activation function output.
+        x_sum = np.dot(
+            np.append(x, self.bias), self.weights
+        )  # the bias input is inserted at last term of  x array, to use its as last input of array
+        return self.sigmoid(x_sum)
